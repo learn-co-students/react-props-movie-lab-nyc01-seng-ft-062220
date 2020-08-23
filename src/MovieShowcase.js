d7d21fe676006dd858a208c5c58b01c0e5558384
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-08-22 21:15:03 -0400

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import MovieCard from './card-components/MovieCard.js'
 import movieData from './data.js'
 
+//spent a long time on this - ended up looking at solution to get over some syntax stuff
+
 export default class MovieShowcase extends Component {
 
-  generateMovieCards = () => {
+  generateMovieCards = () =>
     // map over your movieData array and return an array of the correct JSX
-    return movieData.map(movieObj => <MovieCard title={movieObj.title} IMDBRating={movieObj.IMDBRating} genres={movieObj.genres} poster={movieObj.poster} />)
-  }
+    movieData.map(movieObj => <MovieCard title={movieObj.title} IMDBRating={movieObj.IMDBRating} genres={movieObj.genres} poster={movieObj.poster} />)
+  
 
   render() {
     return (
